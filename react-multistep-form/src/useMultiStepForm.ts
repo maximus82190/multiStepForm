fe44d7f4877338511e5a3ebbcb632c2c6b274c4d
maximus82190 <maxelandaniels@gmail.com>
2022-10-23 12:31:22 +0300

    Add hooks and returns

--- react-multistep-form/src/useMultiStepForm.ts
+++ react-multistep-form/src/useMultiStepForm.ts
@@ -24,6 +24,8 @@ export function useMultistepForm(steps: ReactElement[]) {
     currentStepIndex,
     step: steps[currentStepIndex],
     steps,
+    isFirstStep: currentStepIndex === 0,
+    isLastStep: currentStepIndex === steps.length - 1,
     goTo,
     next,
     back,
