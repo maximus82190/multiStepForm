53b2791e35a3e4d83f7871529413a7973032bb42
maximus82190 <maxelandaniels@gmail.com>
2022-10-23 12:36:23 +0300

    Create AccountForm

---
+++ react-multistep-form/src/AccountForm.tsx
@@ -0,0 +1,10 @@
+export function AccountForm() {
+  return (
+    <>
+      <label>Email</label>
+      <input autoFocus required type="text" />
+      <label>Password</label>
+      <input required type="password" />
+    </>
+  );
+}
