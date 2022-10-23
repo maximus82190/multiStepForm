3043db76054064d83772dc86c2874fb0d201b9de
maximus82190 <maxelandaniels@gmail.com>
2022-10-23 12:31:07 +0300

    Create Functional buttons

--- react-multistep-form/src/App.tsx
+++ react-multistep-form/src/App.tsx
@@ -1,6 +1,7 @@
 import { useMultistepForm } from "./useMultiStepForm";
 function App() {
-  const { steps, currentStepIndex } = useMultistepForm([]);
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultistepForm([<div>One</div>, <div>Two</div>]);
   return (
     <div
       style={{
@@ -16,6 +17,24 @@ function App() {
       <form>
         <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
           {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div
+          style={{
+            marginTop: "1rem",
+            display: "flex",
+            gap: ".5rem",
+            justifyContent: "flex-end",
+          }}
+        >
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="button" onClick={next}>
+            {isLastStep ? "Finish" : "Next"}
+          </button>
         </div>
       </form>
     </div>
